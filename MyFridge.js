dc4475494458ddf121c86c35375d5caf2cf2954d
Yanis Urbis <yanis.urbis@gmail.com>
2016-04-09 15:47:33 +0300

    Basic fridge with images.

---
+++ MyFridge.js
@@ -0,0 +1,53 @@
+Products = new Mongo.Collection('products')
+
+if (Meteor.isClient) {
+
+  Template.fridge.helpers({
+    products: function() {
+      return Products.find({
+        place: 'fridge'
+      })
+    }
+  })
+
+  Template.productList.helpers({
+    products: function() {
+      return Products.find({
+        place: 'supermarket'
+      })
+    }
+  })
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    Products.remove({})
+
+    Products.insert({
+      name:'Milk',
+      img:'/milk.png',
+      place:'fridge'
+    })
+
+    Products.insert({
+      name:'Juice',
+      img:'/juice.png',
+      place:'fridge'
+    })
+
+    Products.insert({
+      name:'Banana',
+      img:'/banana.png',
+      place:'supermarket'
+    })
+
+    Products.insert({
+      name:'Bread',
+      img:'/bread.png',
+      place:'supermarket'
+    })
+
+  })
+}
